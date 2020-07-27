cb459e7ba8a234c429ea43b305dda0aeeb9d3ca6
Marco Bruno <marco.bruno.br@gmail.com>
2020-07-28 01:21:41 +0200

    :tada: Primeiro commit para comunidade

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HeaderFired from './components/HeaderFired';
+import LogoFired from './components/LogoFired';
+
+function App() {
+  return (
+    <HeaderFired>
+      <LogoFired />
+    </HeaderFired>
+  );
+}
+
+export default App;
---
+++ src/components/LogoFired/index.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import logo from '../../assets/img/Logo.png';
+
+const LogoFired = styled.img.attrs({ src: logo, alt: 'Logo da FiredLabs' })`
+    height: 40px;
+`;
+
+export default LogoFired;
