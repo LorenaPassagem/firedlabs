5573523e9fc412c0cc1d00ae5895582831fb8905
Marco Bruno <marco.bruno.br@gmail.com>
2020-07-28 02:28:13 +0200

    :sparkles: Finaliza header e footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import HeaderFired from './components/HeaderFired';
 import LogoFired from './components/LogoFired';
+import ButtonFired from './components/ButtonFired';
+import FooterFired from './components/FooterFired';
+import LinkFired from './components/LinkFired';
+import HighlightFired from './components/HighlightFired';
 
 function App() {
   return (
-    <HeaderFired>
-      <LogoFired />
-    </HeaderFired>
+    <>
+      <HeaderFired>
+        <LogoFired />
+
+        <ButtonFired>Novo vídeo</ButtonFired>
+      </HeaderFired>
+
+      <FooterFired>
+        <LogoFired />
+        <p>
+          Site feito na <HighlightFired>#ImersãoReact</HighlightFired> da {' '}
+          <LinkFired href="https://alura.com.br">Alura</LinkFired>
+        </p>
+      </FooterFired>
+    </>
   );
 }
 
