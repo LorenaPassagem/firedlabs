e60a7782c0aa0d871abda2b81f9f6a412c322374
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-08 01:08:54 +0200

    ✨Add SectionFired in storybook

---
+++ src/stories/SectionFired.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SectionFired from '../components/SectionFired';
+import DescriptionFired from '../components/DescriptionFired';
+
+export default {
+  title: 'SectionFired',
+  component: SectionFired,
+};
+
+export const Default = () => (
+  <SectionFired>
+    <DescriptionFired>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, pariatur
+      soluta neque laboriosam officiis fugit molestias facilis sunt nihil ullam
+      possimus ratione odit ab sequi nisi laudantium impedit. Obcaecati, iusto.
+    </DescriptionFired>
+  </SectionFired>
+);
