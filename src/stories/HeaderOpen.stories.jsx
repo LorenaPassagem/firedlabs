e95ec6e34b8230f7064a4cef215ba76e5992d17c
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-08 00:50:36 +0200

    ✨Add the HeaderOpen in storybook

---
+++ src/stories/HeaderOpen.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import HeaderOpen from '../components/HeaderOpen';
+
+export default {
+  title: 'HeaderOpen',
+  component: HeaderOpen,
+};
+
+export const Default = () => <HeaderOpen />;
