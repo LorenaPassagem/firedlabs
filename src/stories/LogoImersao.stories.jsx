78508486ee9a9c79f5afb9977ef9123221f50382
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-08 01:05:41 +0200

    ✨Add LogoImersao in storybook

---
+++ src/stories/LogoImersao.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LogoImersao from '../components/LogoImersao';
+
+export default {
+  title: 'LogoImersao',
+  compoentn: LogoImersao,
+};
+
+export const Default = () => <LogoImersao />;
