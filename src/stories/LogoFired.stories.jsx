5dde7f4a03f08dd915a8d3f7a6f7244d1a1aa961
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-08 01:03:46 +0200

    ✨Add LogoFired in storybook

---
+++ src/stories/LogoFired.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LogoFired from '../components/LogoFired';
+
+export default {
+  title: 'LogoFired',
+  component: LogoFired,
+};
+
+export const Default = () => <LogoFired />;
