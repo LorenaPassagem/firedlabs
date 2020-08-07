7373eff610fd95b5b0d9276b8e64ad3098e6bb16
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-08 01:10:30 +0200

    ✨Add TagFired in storybook

---
+++ src/stories/TagFired.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TagFired from '../components/TagFired';
+
+export default {
+  title: 'TagFired',
+  component: TagFired,
+};
+
+export const Default = () => <TagFired>Texto feliz</TagFired>;
