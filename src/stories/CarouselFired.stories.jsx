3d0841fd6de794f80b26e61a1967cebb82f013b9
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-08 00:42:24 +0200

    ✨Add CarouselFired in storybook

---
+++ src/stories/CarouselFired.stories.jsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import CarouselFired from '../components/CarouselFired';
+import kikano from './assets/img/kikano.png';
+import lucasMelo from './assets/img/lucas-melo.png';
+
+export default {
+  title: 'CarouselFired',
+  component: CarouselFired,
+};
+
+const videos = [
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+  {
+    src: kikano,
+    alt: 'Boas vindas a FiredLabs',
+    title: 'Boa vindas a FiredLabs',
+    avatar: lucasMelo,
+    channelName: 'Lucasmml',
+    timer: '00:01:00',
+    link: 'https:"//twitch.tv/lucasmml',
+  },
+];
+
+export const Default = () => <CarouselFired videos={videos} />;
