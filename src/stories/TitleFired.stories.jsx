f824d215c78ccaf764013ee51369e53ab3c6681a
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-08 01:28:15 +0200

    ✨Add TitleFired in storybook

---
+++ src/stories/TitleFired.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import TitleFired from '../components/TitleFired';
+
+export default {
+  title: 'TitleFied',
+  component: TitleFired,
+};
+
+export const Default = () => <TitleFired>Texto do título</TitleFired>;
