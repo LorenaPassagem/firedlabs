2f22a884f03a58b9ebffcaa34cc02908094e00ca
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-08 00:45:09 +0200

    ✨Add DescriptionFired in storybook

---
+++ src/stories/DescriptionFired.stories.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import DescriptionFired from '../components/DescriptionFired';
+
+export default {
+  title: 'DescriptionFired',
+  component: DescriptionFired,
+};
+
+export const Default = () => (
+  <DescriptionFired>
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed nihil soluta
+    consequatur similique pariatur, ratione maiores nisi quidem corporis
+    consectetur repellat commodi voluptatem dolore, cupiditate autem ad
+    excepturi consequuntur distinctio?
+  </DescriptionFired>
+);
