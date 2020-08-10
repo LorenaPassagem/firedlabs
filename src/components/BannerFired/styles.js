700fbe6c0d216d70e7696314562a5a8ec72f290d
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-10 23:45:58 +0200

    🚧Add breakpoint 1300 to BannerFired

--- src/components/BannerFired/styles.js
+++ src/components/BannerFired/styles.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 import container from '../../styles/tools/container';
 import TagFired from '../TagFired';
 import TitleFired from '../TitleFired';
+import DescriptionFired from '../DescriptionFired';
+import { WrapperThumb } from '../ThumbFired/styles';
 
 export const Text = styled.div`
   padding-top: 10rem;
@@ -14,6 +16,16 @@ export const Text = styled.div`
   ${TitleFired} {
     margin-bottom: 20rem;
   }
+
+  @media (max-width: 1300px) {
+    box-sizing: border-box;
+    width: 468px;
+    max-width: none;
+
+    & > ${TitleFired}, & > ${DescriptionFired} {
+      display: none;
+    }
+  }
 `;
 
 export const BannerStyle = styled.section`
@@ -23,4 +35,14 @@ export const BannerStyle = styled.section`
   align-items: center;
   max-height: 70vh;
   height: 100vh;
+
+  @media (max-width: 1300px) {
+    flex-direction: column;
+    justify-content: flex-start;
+    margin-bottom: 50rem;
+
+    & ${WrapperThumb} {
+      width: 500px;
+    }
+  }
 `;
