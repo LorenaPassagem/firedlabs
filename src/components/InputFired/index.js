6a8c3005fd480c2aeceb1d37569210667bea4a99
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-17 22:33:01 +0200

    ✨Create InputFired component

---
+++ src/components/InputFired/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const InputFired = styled.input`
+  box-sizing: border-box;
+  font-size: 30rem;
+  padding: 8rem 10rem;
+  font-family: inherit;
+  max-width: 640px;
+  width: 100%;
+  border: 2px solid var(--color-fiery-rose);
+  color: var(--color-eerie-black);
+`;
+
+export default InputFired;
---
+++ src/stories/InputFired.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import InputFired from '../components/InputFired';
+
+export default {
+  title: 'InputFired',
+  component: InputFired,
+};
+
+export const Default = () => <InputFired placeholder="Email aqui" />;
