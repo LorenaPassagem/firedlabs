556cbeace295c189575abf87619412cfd116cd60
Marco Bruno <marco.bruno.br@gmail.com>
2020-08-17 22:18:41 +0200

    ✨Create LabelFired component

---
+++ src/components/LabelFired/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const LabelFired = styled.label`
+  text-align: left;
+  font-size: 40rem;
+  color: var(--color-floral-white);
+  margin-bottom: 5rem;
+`;
+
+export default LabelFired;
---
+++ src/stories/LabelFired.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import LabelFired from '../components/LabelFired';
+
+export default {
+  title: 'LabelFired',
+  component: LabelFired,
+};
+
+export const Default = () => <LabelFired>Conteúdo da label</LabelFired>;
